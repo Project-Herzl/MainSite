07aa6106fb1224f1590e2a5b825f9ad4a99e3531
AYMBotBuildersOfficial <AutoTranslateAdministrator@proton.me>
2024-08-15 12:28:24 +0530

    Update background2.js

--- background2.js
+++ background2.js
@@ -1,19 +1,10 @@
-let numberofturns = 0;
 let streams = [];
-const fadeInterval = 1.6;
 const symbolSize = 14;
-const randomfps = [20, 70, 1000, 5000, 10, 7734, 3283];
-let index;
-let newrandom = [];
-let deathSound;
+let messageCounter = 0;
 
 function setup() {
-  createCanvas(
-    window.innerWidth,
-    window.innerHeight);
-  deathSound.play()
+  createCanvas(window.innerWidth, window.innerHeight);
   background(0);
-
   let x = 0;
   for (let i = 0; i <= width / symbolSize; i++) {
     let stream = new Stream();
@@ -21,26 +12,26 @@ function setup() {
     streams.push(stream);
     x += symbolSize;
   }
-
   textFont('Consolas');
   textSize(symbolSize);
 }
 
 function draw() {
-  index = int(random(0, 7));
-  numberofturns++;
-
-  if (numberofturns % 100) {
-    background(0);  
-        fill('red');
-    textSize(20);
-    textAlign(CENTER, CENTER);
-    text('HAMAS WILL FALL', width / 2, height / 2);
-  }
-
+  background(0, 150); 
+  
+  drawMessage(); 
+  
   streams.forEach(function(stream) {
     stream.render();
   });
+}
+
+function drawMessage() {
+  fill('red');
+  textSize(32);
+  textAlign(CENTER, TOP);
+  textStyle(BOLD);
+  text('HAMAS WILL FALL', width / 2, 20); 
 }
 
 function Matrix(x, y, speed, first) {
@@ -52,12 +43,11 @@ function Matrix(x, y, speed, first) {
   this.switchInterval = round(random(2, 25));
 
   this.setToRandomSymbol = function() {
-    let charType = round(random(0, 5));
     if (frameCount % this.switchInterval === 0) {
-      if (charType > 1) {
+      if (random() > 0.5) {
         this.value = String.fromCharCode(0x30A0 + floor(random(0, 97)));
       } else {
-        this.value = floor(random(0,10));
+        this.value = floor(random(0, 10));
       }
     }
   }
@@ -73,14 +63,9 @@ function Stream() {
   this.speed = random(5, 22);
 
   this.generateSymbols = function(x, y) {
-    let first = round(random(0, 1)) == 1;
-    for (let i =0; i <= this.totalSymbols; i++) {
-      let symbol = new Matrix(
-        x,
-        y,
-        this.speed,
-        first
-      );
+    let first = random() < 0.5;
+    for (let i = 0; i <= this.totalSymbols; i++) {
+      let symbol = new Matrix(x, y, this.speed, first);
       symbol.setToRandomSymbol();
       this.symbols.push(symbol);
       y -= symbolSize;
@@ -99,5 +84,18 @@ function Stream() {
       symbol.rain();
       symbol.setToRandomSymbol();
     });
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  background(0);
+  streams = [];
+  let x = 0;
+  for (let i = 0; i <= width / symbolSize; i++) {
+    let stream = new Stream();
+    stream.generateSymbols(x, random(-2000, 0));
+    streams.push(stream);
+    x += symbolSize;
   }
 }
